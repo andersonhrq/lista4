3f137f7d591287aae9408ec4c422294277be56e0
andersonhrq <138603258+andersonhrq@users.noreply.github.com>
2023-07-04 12:31:34 -0300

    Add files via upload

---
+++ lista4_q1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+string string;
+    cout << "Digite uma string: ";
+    getline(cin, string);
+    cout << "A string digatada é: " << string;
+}
